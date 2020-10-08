552e2dfe41e2dc18a24d14d31560d393bb696d48
Rafael Macedo <rafaelldemacedo@gmail.com>
2020-10-08 16:04:01 -0300

    Removido arquivos

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Projeto Framework</title>
+    <title>Teste Kabum</title>
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/sb-admin.css" rel="stylesheet">
@@ -26,11 +26,9 @@
 <body>
 
     <div id="wrapper">
-        <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <a class="navbar-brand" href="index.php">Projeto Framework</a>
+                <a class="navbar-brand" href="index.php">Teste Kabum</a>
             </div>
         </nav>
 
@@ -59,24 +57,11 @@
                         </form>
                     </div>
                 </div>
-                <!-- row -->
-
             </div>
-            <!-- container-fluid -->
-
         </div>
-        <!-- page-wrapper -->
-
     </div>
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
     <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
     <script src="js/plugins/morris/raphael.min.js"></script>
     <script src="js/plugins/morris/morris.min.js"></script>
     <script src="js/plugins/morris/morris-data.js"></script>
--- resources/layout/menu.php
+++ resources/layout/menu.php
@@ -4,12 +4,6 @@ if(!isset($_SESSION)){
 }
 ?>
     <div class="navbar-header">
-        <!-- <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button> -->
         <a class="navbar-brand" href="index.php">Teste Kabum</a>
     </div>
     <div class="collapse navbar-collapse navbar-ex1-collapse">
@@ -20,14 +14,6 @@ if(!isset($_SESSION)){
                     <a href="perfil.php"><i class="fa fa-fw fa-dashboard"></i>Perfil</a>
                 </li>
                 <li class="cadastro">
-                    <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i>Cliente<i class="fa fa-fw fa-caret-down"></i></a>
-                    <ul id="demo" class="collapse">
-                        <li class="cadastro_aluno">
-                            <a href="cadastro_aluno.php">Aluno</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="home active">
                     <a href="cliente.php"><i class="fa fa-fw fa-dashboard"></i>cliente</a>
                 </li>
                 <?php
